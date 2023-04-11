c24fee241f24513b3ff5194fcecf8f9ecca11537
melanieSasari <melsasarialborta@gmail.com>
2023-04-11 17:39:01 -0400

    Botones funcionando

--- app/src/main/java/com/example/aqui_estoy/Signin.kt
+++ app/src/main/java/com/example/aqui_estoy/Signin.kt
@@ -1,11 +1,19 @@
 package com.example.aqui_estoy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Signin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
+
+        val btnIrARegistrarse = findViewById<Button>(R.id.button_signin)
+        btnIrARegistrarse.setOnClickListener{
+            val Home: Intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(Home)
+        }
     }
 }
--- app/src/main/java/com/example/aqui_estoy/portada.kt
+++ app/src/main/java/com/example/aqui_estoy/portada.kt
@@ -1,11 +1,28 @@
 package com.example.aqui_estoy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class portada : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_portada)
+
+        val btnIrARegistrarse = findViewById<Button>(R.id.registrarse)
+        val btnIrAIngresar = findViewById<Button>(R.id.ingresar)
+
+        btnIrARegistrarse.setOnClickListener{
+            val ventanaRegistrarse: Intent = Intent(applicationContext, Signin::class.java)
+            startActivity(ventanaRegistrarse)
+        }
+
+        btnIrAIngresar.setOnClickListener{
+            val ventanaIngresar: Intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(ventanaIngresar)
+        }
+
     }
+
 }
