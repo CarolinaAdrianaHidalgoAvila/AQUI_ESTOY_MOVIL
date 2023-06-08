b34751f9bb83d23c4162f56f4c0fe0d1fb24b3a1
Diego Quiñones <diego.quinones.tx@gmail.com>
2023-06-08 02:45:08 -0400

    Added option to redirect to profile fragment

--- app/src/main/java/com/aquiestoy/aqui_estoy/ui/MainActivity.kt
+++ app/src/main/java/com/aquiestoy/aqui_estoy/ui/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.aquiestoy.aqui_estoy.ui
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
@@ -9,8 +12,11 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.aquiestoy.aqui_estoy.Create_Pet_Found
 import com.aquiestoy.aqui_estoy.R
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Inject
@@ -19,10 +25,18 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
-
+    private lateinit var addButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addButton = findViewById(R.id.Add_button)
+
+        addButton.setOnClickListener {
+            Log.d("entered button", "button Big")
+            val windowCreate_Pet_Found = Intent(applicationContext, Create_Pet_Found::class.java)
+            startActivity(windowCreate_Pet_Found)
+        }
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
         navController = navHostFragment.navController
--- app/src/main/java/com/aquiestoy/aqui_estoy/ui/ProfileFragment.kt
+++ app/src/main/java/com/aquiestoy/aqui_estoy/ui/ProfileFragment.kt
@@ -10,6 +10,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.aquiestoy.aqui_estoy.GetUserViewModel
 import com.aquiestoy.aqui_estoy.R
 import com.aquiestoy.domain.User
 import com.squareup.picasso.Picasso
@@ -17,43 +19,43 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
-    lateinit var profilePictureImageView: ImageView
-    lateinit var usernameTextView: TextView
-    lateinit var usertagTextView: TextView
-
-    private val profileViewModel: ProfileViewModel by viewModels()
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
+    private lateinit var viewModel: GetUserViewModel
+    private lateinit var tvFirstName: TextView
+    private lateinit var tvLastName: TextView
+    private lateinit var tvPhone: TextView
+    private lateinit var tvEmail: TextView
+    private lateinit var tvAddress: TextView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view: View = inflater.inflate(R.layout.fragment_profile, container, false)
-        profilePictureImageView = view.findViewById(R.id.imageView_pictureProfile)
-        usernameTextView = view.findViewById(R.id.textView_usernameProfile)
-        usertagTextView = view.findViewById(R.id.textView_usertagProfile)
+        viewModel = ViewModelProvider(this).get(GetUserViewModel::class.java)
 
-        profileViewModel.model.observe(viewLifecycleOwner, Observer (::updateUi))
+        tvFirstName = view.findViewById(R.id.tv_FirstName)
+        tvLastName = view.findViewById(R.id.tv_LastName)
+        tvPhone= view.findViewById(R.id.tv_Phone)
+        tvEmail= view.findViewById(R.id.tv_Email)
+        tvAddress= view.findViewById(R.id.tv_Address)
+        val idUser = "632333ceca137c2c4b95168c"
 
-        profileViewModel.loadUser("632333ceca137c2c4b95168c")
+        viewModel.user.observe(viewLifecycleOwner, { user ->
+            user?.let {
+                actualizarInterfazUsuario(it)
+            }
+        })
+
+        viewModel.fetchUser(idUser)
         return view
     }
-
-    private fun updateUi(uiModel: ProfileViewModel.UiModel?) {
-        when(uiModel) {
-            is ProfileViewModel.UiModel.Content -> showUser(uiModel.user)
-            else -> Toast.makeText(context, "AS", Toast.LENGTH_SHORT).show()
+    private fun actualizarInterfazUsuario(user: User) {
+        requireActivity().runOnUiThread {
+            tvFirstName.text = user.firstName
+            tvLastName.text = user.lastName
+            tvPhone.text = user.phone
+            tvEmail.text = user.email
+            tvAddress.text = user.address
         }
-    }
-
-    private fun showUser(user: User) {
-        if(user.photo != ""){
-            Picasso.get().load(user.photo).into(profilePictureImageView)
-        }
-        usernameTextView.text = user.firstName + " " + user.lastName
-        usertagTextView.text = user.email
     }
 }
