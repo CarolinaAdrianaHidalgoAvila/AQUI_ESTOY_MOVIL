afeb0bf3bf46331160f0484b17f3ab452426d38b
unknown <caroh2001@gmail.com>
2023-06-08 21:54:11 -0400

    login

--- app/src/main/java/com/aquiestoy/aqui_estoy/LoginActivity.kt
+++ app/src/main/java/com/aquiestoy/aqui_estoy/LoginActivity.kt
@@ -4,20 +4,44 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.aquiestoy.aqui_estoy.ui.MainActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var editTextEmail: EditText
+    lateinit var editTextPassword: EditText
+    lateinit var ingresar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        editTextEmail = findViewById(R.id.editText_email)
+        editTextPassword = findViewById(R.id.editText_password)
+        ingresar = findViewById(R.id.button_login)
 
-        val btnIrAIngresar = findViewById<Button>(R.id.button_login)
+        ingresar.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
 
-        btnIrAIngresar.setOnClickListener{
-            val Home: Intent = Intent(applicationContext, MainActivity::class.java)
-            startActivity(Home)
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Inicio de sesión exitoso, el usuario ha iniciado sesión
+                        val user = FirebaseAuth.getInstance().currentUser
+
+                        // Redirige al usuario a la actividad principal
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    } else {
+                        // Error en el inicio de sesión
+                        Toast.makeText(this, "Error en el inicio de sesión.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
         }
-    }
+        }
 
-}
+    }
--- app/src/main/java/com/aquiestoy/aqui_estoy/Signin.kt
+++ app/src/main/java/com/aquiestoy/aqui_estoy/Signin.kt
@@ -3,18 +3,46 @@ package com.aquiestoy.aqui_estoy
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.aquiestoy.aqui_estoy.ui.MainActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 
 class Signin : AppCompatActivity() {
+    lateinit var editTextEmail: EditText
+    lateinit var editTextPassword: EditText
+    lateinit var registrarse: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
+        FirebaseApp.initializeApp(this)
 
-        val btnIrARegistrarse = findViewById<Button>(R.id.button_signin)
-        btnIrARegistrarse.setOnClickListener{
-            val Home: Intent = Intent(applicationContext, MainActivity::class.java)
-            startActivity(Home)
+        editTextEmail = findViewById(R.id.siginText_email)
+        editTextPassword = findViewById(R.id.siginText_password)
+        registrarse = findViewById(R.id.button_signin)
+
+
+        registrarse.setOnClickListener{
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Registro exitoso, el usuario se ha creado
+                        val user = FirebaseAuth.getInstance().currentUser
+                        // Puedes realizar acciones adicionales, como enviar un correo de verificación
+
+                        // Redirige al usuario a la actividad de inicio de sesión
+                        startActivity(Intent(this, LoginActivity::class.java))
+                        finish()
+                    } else {
+                        // Error en el registro
+                        Toast.makeText(this, "Error en el registro de usuario.", Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
     }
 }
